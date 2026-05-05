5d68ddb8d2d2463683a45acd81c51d4cfa672885
Saurav Pal <node.dev1@softinsystem.com>
2026-05-05 16:24:03 +0530

    Added spvcs class

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# git
+The Core Idea: Content‑Addressable Storage
+Most databases store data by location (e.g., "file at path X").
+Git stores data by content — the name of an object is a hash of its content.
+
+----------------------------------------------
+text
+hash = SHA1( content )
+----------------------------------------------
+
+That means:
+
+If you change a single byte, the hash changes completely.
+
+If two files have identical content, they are stored only once.
+
+Every object (file, directory, commit) is retrieved by its hash.
+
+This is the only data structure Git has: a key‑value store where the key is the hash and the value is the object.
---
+++ spvcs.py
@@ -0,0 +1,36 @@
+#! usr/bin/env python3
+import sys, os, hashlib, zlib
+from pathlib import Path
+from datetime import datetime
+
+class SPVCS:
+    def __init__(self,repo_path='.'):
+        self.repo_path = Path(repo_path).absolute()
+        self.spvcs_dir = self.repo_path / '.spvcs'
+
+    def init(self):
+        """ Create .spvcs directory and subdirectories """
+        if self.spvcs_dir.exists():
+            print("Repository already exists.")
+            return
+        # create objects/ and refs/heads/
+        (self.spvcs_dir/'objects').mkdir(parents=True)
+        (self.spvcs_dir/'refs'/'heads').mkdir(parents=True)
+        with open(self.spvcs_dir/'HEAD','w') as f:
+            f.write('ref: refs/heads/master\n')
+        print(f"Initialized empty SPVCS repoository in {self.spvcs_dir}")
+
+def main():
+    if (len(sys.argv)<2):
+        print("Usage: spvcs <command> [args]")
+        return
+    cmd = sys.argv[1]
+    repo = SPVCS()
+
+    if cmd == 'init':
+        repo.init()
+    else:
+        print(f"Unknown command: {cmd}")
+
+if __name__ == "__main__":
+    main()
